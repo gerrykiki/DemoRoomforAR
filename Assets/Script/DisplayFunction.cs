d76df3b90c9e47db7a01593839b9e7606088cab7
Gerry PW Lin <gerry_pw_lin@wistron.com>
2018-08-31 16:26:23 +0800

    first commit

---
+++ Assets/Script/DisplayFunction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayFunction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# DemoRoomforAR
