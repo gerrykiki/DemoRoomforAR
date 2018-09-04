6aa54c77d3f7ab0fa0e88d59e9abe023581283ce
Gerry PW Lin <gerry_pw_lin@wistron.com>
2018-09-04 10:40:12 +0800

    Issue:Function Des:Cube Move with Mouse

--- Assets/Script/DisplayFunction.cs
+++ Assets/Script/DisplayFunction.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DisplayFunction : MonoBehaviour {
-
+	private bool isMouseDrag;
+	public GameObject ModelTarget;
 	// Use this for initialization
 	void Start () {
-		
+		isMouseDrag = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButtonDown(0))
+		{
+			isMouseDrag = true;
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			isMouseDrag = false;
+		}
+
+		if (isMouseDrag)
+		{
+			Ray MouseRayPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit MouseRayHit;
+			if(Physics.Raycast(MouseRayPoint,out MouseRayHit))
+			{
+				Vector3 mousePoint = Input.mousePosition;
+				mousePoint.z = 20;
+				Vector3 WorldMousePoint = Camera.main.ScreenToWorldPoint(mousePoint);
+				ModelTarget.transform.position = WorldMousePoint;
+			}
+			
+		}
 	}
 }
